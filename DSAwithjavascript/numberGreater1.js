dc9758c9e972df665ddb407756767d4156ea5039
shivam birla <shivambirla497@gmail.com>
2023-04-29 15:03:39 +0530

    28th april dsa

---
+++ DSAwithjavascript/numberGreater1.js
@@ -0,0 +1,19 @@
+let a=1000
+let b=10
+let c=100
+if(a>b){
+    
+  console.log(a)
+}
+
+else if(b>c){
+    console.log(b)
+}
+else if(c>a)
+{
+    console.log(c)
+}
+else
+{
+    console.log("no difference")
+}
