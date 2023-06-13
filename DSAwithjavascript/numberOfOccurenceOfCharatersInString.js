37c5b8dd5b18b30cf34ca6e9c132de0bf82dc2b7
shivam birla <shivambirla497@gmail.com>
2023-06-13 17:17:47 +0530

    13 june DSA

---
+++ DSAwithjavascript/numberOfOccurenceOfCharatersInString.js
@@ -0,0 +1,9 @@
+let str='obamgjruerydcbshwoqpeprodjcnxsbhdhfndjgwryiupypbmvcnxbqieeuryyzbavsdhfiwowpwcha'
+let x='b'
+let count=0
+for(let i=0;i<str.length;i++){
+    if(str[i]===x){
+count++
+    }
+    }
+    console.log(count)
