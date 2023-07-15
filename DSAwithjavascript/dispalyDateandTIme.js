ad0672fc3e5cdbf64a6ccdf9b6fb3750a528a8c6
shivam birla <shivambirla497@gmail.com>
2023-07-15 12:42:36 +0530

    14th july dsa

---
+++ DSAwithjavascript/dispalyDateandTIme.js
@@ -0,0 +1,8 @@
+const date = new Date();
+
+const hours = date.getHours();
+const minutes = date.getMinutes();
+const seconds = date.getSeconds();
+const milliseconds = date.getMilliseconds();
+const dat=date.getDay()
+console.log(hours,minutes, seconds,dat);
